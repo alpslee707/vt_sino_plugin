911605de882f9f703e89dbd192e7ad43f6301202
Nature Lee <leetr.nature@hotmail.com>
2017-09-08 18:06:48 +0800

    Create vt_sino_plugin.js

---
+++ public/vt_sino_plugin.js
@@ -0,0 +1,5 @@
+import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
+
+import echartslineVisTypeProvider from 'plugins/vt_sino_plugin/echartsline';
+
+VisTypesRegistryProvider.register(echartslineVisTypeProvider);
